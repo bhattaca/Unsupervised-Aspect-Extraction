aa463f8a1ad5fb95f1f3ac8c0195cc945bc41cdd
Arindam Bhattacharya <arindam@uber.com>
2019-06-29 00:13:46 -0700

    Update train.py

--- code/train.py
+++ code/train.py
@@ -175,12 +175,9 @@ for ii in range(args.epochs):
             desc = aspect_emb[ind]
             sims = word_emb.dot(desc.T)
             ordered_words = np.argsort(sims)[::-1]
-            print(ordered_words[:10])
-            print(vocab_inv[56])
-            print(sims[56])
             desc_list = [vocab_inv[w] + ":" + str(sims[w]) for w in ordered_words[:100]]
             print('Aspect %d:' % ind)
-            print(desc_list)
+            print('Desc list:',desc_list)
             aspect_file.write('Aspect %d:\n' % ind)
             aspect_file.write(' '.join(desc_list) + '\n\n')
 

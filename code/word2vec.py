5396b3b6168149cb2746739d7eae06cc1b853a72
Arindam Bhattacharya <arindam@uber.com>
2019-07-09 00:51:40 -0700

    Update word2vec.py

--- code/word2vec.py
+++ code/word2vec.py
@@ -20,10 +20,10 @@ def main(domain):
     model_file = '/mnt/cephfs/hadoop-compute/phoenix/arindam/projectKraken/data/unsupervised_aspect_data/preprocessed_data/%s/w2v_embedding' % domain
     sentences = Sentences(source)
 
-    model = FastText(min_count=1)
+    model = FastText(size=200, window=3, min_count=100)
     model.build_vocab(sentences)
     model.train(sentences, total_examples=model.corpus_count, epochs=model.iter)
-    print(model)
+    print("MODEL INFO : ",model)
     #model = gensim.models.Word2Vec(sentences, size=200, window=5, min_count=10, workers=4, sg=1, iter=5)
     model.save(model_file)
 

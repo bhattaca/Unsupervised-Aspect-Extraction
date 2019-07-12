c6abd909a1e1fa315b61c6b498ef335485173f82
Arindam Bhattacharya <arindam@uber.com>
2019-07-12 13:20:02 -0700

    Update word2vec.py

--- code/word2vec.py
+++ code/word2vec.py
@@ -20,7 +20,7 @@ def main(domain):
     model_file = '/mnt/cephfs/hadoop-compute/phoenix/arindam/projectKraken/data/unsupervised_aspect_data/preprocessed_data/%s/w2v_embedding.bin' % domain
     sentences = Sentences(source)
 
-    model = FastText(size=200, window=3, min_count=5)
+    model = FastText(size=300, window=4, min_count=10)
     model.build_vocab(sentences)
     model.train(sentences, total_examples=model.corpus_count, epochs=model.iter)
     print("MODEL INFO : ",model)
